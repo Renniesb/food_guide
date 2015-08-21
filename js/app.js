07155bb995ef77f5c3949dd4b59c84b5fc83f9f7
Rennie Bevineau <rsbevineau@gmail.com>
2015-08-21 11:09:49 -0600

    test collection can be printed out

--- js/app.js
+++ js/app.js
@@ -1,58 +1,53 @@
 $(function(){
 
-	var SearchList = Backbone.Collection.extend({
-		url: "https://api.nutritionix.com/v1_1/search/taco?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=26952a04&appKey=78e2b31849de080049d26dc6cf4f338c",
+var SearchList = Backbone.Collection.extend({
+    url: "https://api.nutritionix.com/v1_1/search/taco?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=26952a04&appKey=78e2b31849de080049d26dc6cf4f338c",
 
-		initialize: function(){
-			this.bind("reset", function(model, options){
-			console.log("Inside event");
-			console.log(model);
-			});
-		}
+    initialize: function(){
+        this.bind("reset", function(model, options){
+        console.log("Inside event");
+        console.log(model);
+        });
+    }
 
-	});
+});
 
-	var terms = new SearchList();
+// The main view of the application
+var App = Backbone.View.extend({
 
-	terms.fetch({
-		success: function(response,xhr) {
-			 console.log("Inside success");
-			 console.log(response.toJSON());
-		},
-		ERROR: function (errorResponse) {
-		       console.log(errorResponse)
-		}
-	});
+    initialize: function () {
 
+        this.collection = new SearchList();
 
-	// The main view of the application
-	var App = Backbone.View.extend({
+        this.collection.fetch({
+            success: function (response, xhr) {
+                console.log("Inside success");
+                console.log(response.toJSON());
+            },
+            ERROR: function (errorResponse) {
+                console.log(errorResponse)
+            }
+        });
 
-		// Base the view on an existing element
-		el: $('.container'),
-
-		initialize: function(){
-
-			this.listenTo(this.model, 'sync', this.render);
-
-			// Cache these selectors
-			// this.total = $('#total span');
-			this.list = $('#listing');
+        this.listenTo(this.collection, 'sync', this.render);
 
 
-		},
+        this.list = $('#listing');
 
-		render: function(){
 
-			// Calculate the total order amount by agregating
-			// the prices of only the checked elements
-			terms.each(function(term){
+    },
 
-				this.list.append("<li>"+ term.get('field[brand_name]')+"</li>");
+    render: function(){
 
-			}, this);
+        var context = this;
+        this.collection.each(function (term) {
+            _.each(term.get('hits'), function (item) {
+                context.list.append("<li>" + item.fields.brand_name + "</li>");
+            });
 
-		}
-	});
+        }, this);
 
+    }
+});
+        new App();
 });
