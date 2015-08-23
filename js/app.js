25a086fb1b8409a0305ab8f1014c2d19e7aa1ee2
Rennie Bevineau <rsbevineau@gmail.com>
2015-08-23 14:32:01 -0600

    add calorie amount

--- js/app.js
+++ js/app.js
@@ -20,27 +20,29 @@ var SearchList = Backbone.Collection.extend({
 // The main view of the application
 var App = Backbone.View.extend({
 
+    el: 'body',
 
+    events: {
+    "input #searchBox" : "prepCollection",
+    "click li" : "track"
+    },
 
     initialize: function () {
 
         this.model = new SearchList();
+        this.prepCollection =_.debounce(this.prepCollection, 1000);
 
-        this.list = $('#listing');
+        this.$list = $('#listing');
+        // this.saved =$('#tracked');
 
     },
-    el: 'input',
 
-    events: {
-
-    "keyup" : "prepCollection"
-    },
     prepCollection: function(){
         var name = $('input').val();
-        var newUrl = "https://api.nutritionix.com/v1_1/search/" + name + "?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=26952a04&appKey=33b9262901d0068d4895736b5af19805";
+        var newUrl = "https://api.nutritionix.com/v1_1/search/" + name + "?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name,brand_name,item_id,nf_calories&appId=26952a04&appKey=33b9262901d0068d4895736b5af19805";
 
        if (name == ""){
-        this.list.html("")
+        this.$list.html("")
        }
        else{
         this.model.url = newUrl;
@@ -58,15 +60,22 @@ var App = Backbone.View.extend({
 
     },
 
+    // track: function(){
+    // },
+
+
     render: function(){
         var terms = this.model;
         var wordhtml = "";
         terms.each(function (term) {
-            wordhtml = wordhtml + "<li>" +"Food Name: " + term.get('fields')["item_name"] + '<br/>' +'Brand Name: '+ term.get('fields')["brand_name"] + "</li>"
+            wordhtml = wordhtml + "<li>" +"<strong>" + term.get('fields')["item_name"] + '</strong>'+ ' ('+ term.get('fields')["brand_name"] + ')'+' - '+ term.get('fields')["nf_calories"] + ' Calories' + "</li>"
         }, this);
-        this.list.html(wordhtml);
+        this.$list.html(wordhtml);
 
     }
 });
        var app = new App();
 });
+
+
+// TODO: click event triggers render of saved elements with calorie count
