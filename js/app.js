6ef54e99a3f495176265a321bf699be7659832b7
Rennie Bevineau <rsbevineau@gmail.com>
2015-08-26 02:28:13 -0600

    completed app

--- js/app.js
+++ js/app.js
@@ -39,7 +39,8 @@ var App = Backbone.View.extend({
     events: {
     "input #searchBox" : "prepCollection",
     "click #listing li" : "track",
-    "click button": "buttonClicked"
+    "click #add": "addClicked",
+    "click #remove": "removeClicked"
     },
 
     initialize: function () {
@@ -49,12 +50,13 @@ var App = Backbone.View.extend({
         this.prepCollection =_.debounce(this.prepCollection, 1000);
         this.$total = $('#total span');
         this.$list = $('#listing');
+        this.$instruct = $('#instruct');
         this.$tracked =$('#tracked');
 
 
     },
 
-    buttonClicked: function(e) {
+    addClicked: function(e) {
         var $target = $(e.currentTarget).parent();
        var $selected = $target.find('#mySelect').val();
 
@@ -84,14 +86,23 @@ var App = Backbone.View.extend({
 
     },
 
+    removeClicked: function (e) {
+        var $target = $(e.currentTarget).parent();
+        var removeid = $target.attr('data-id');
+        $target.remove();
+        this.foods.remove(removeid);
+    },
+
     prepCollection: function(){
         var name = $('input').val();
         var newUrl = "https://api.nutritionix.com/v1_1/search/" + name + "?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name,brand_name,item_id,nf_calories&appId=26952a04&appKey=33b9262901d0068d4895736b5af19805";
 
        if (name == ""){
         this.$list.html("")
+        this.$instruct.html("")
        }
        else{
+        this.$instruct.html("Click On A Food Item To Track It")
         this.model.url = newUrl;
         this.model.fetch({
             success: function (response, xhr) {
@@ -119,7 +130,7 @@ var App = Backbone.View.extend({
         var calorieAmt = parseFloat(calorieString);
         var foodid = $target.attr('data-id');
 
-        var chooseday ='<form>What meal was this part of?: <select id="mySelect"> <option value="Breakfast">Breakfast</option><option value="Lunch">Lunch</option><option value="Dinner">Dinner</option><option value="Snack">Snack</option></select></form><button type="button">Add To Meal</button>';
+        var chooseday ='<form>What meal was this part of?: <select id="mySelect"> <option value="Breakfast">Breakfast</option><option value="Lunch">Lunch</option><option value="Dinner">Dinner</option><option value="Snack">Snack</option></select></form><button id="add" type="button">Add To Meal</button><button id="remove" type="button">Remove From Tracked</button>';
 
         var trackedhtml = '<li'+' data-id='+'"'+ foodid +'"'+'>' +"<strong>" + item_name + '</strong>'+ ' ('+ brand_name + ')'+' - '+ calorieAmt + ' Calories' + chooseday + '</li>'
 
